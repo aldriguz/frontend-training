fb908aea6b213e23c164ab2f29e1d9d660cc72cd
Kevin Alama <alama1992@gmail.com>
2020-09-09 08:16:19 -0500

    first version todo list vanilla js

---
+++ DOM_manipulation/app.js
@@ -0,0 +1,35 @@
+var button = document.getElementById('enter')
+var input = document.getElementById("user-input");
+var ul = document.querySelector('ul')
+var li = document.querySelector('li');
+
+function inputLength() {
+    return input.value.length;
+}
+
+function createListElement() {
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = '';
+}
+
+function addListAfterClick(){
+    if(inputLength() > 0){
+        createListElement();
+    }
+}
+
+function addListAfterKeyPress(event){
+    if(inputLength() > 0 && event.keyCode === 13){
+        createListElement();
+    }
+}
+
+
+/* events */
+button.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterKeyPress);
+li.addEventListener('click', function(){
+    console.log('item clickeado');
+})
