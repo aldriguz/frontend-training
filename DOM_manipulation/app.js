158c689a7315c1615ed53001ca483e0ad2194ca5
Kevin Alama <alama1992@gmail.com>
2020-09-10 09:30:13 -0500

    todo example completed

--- DOM_manipulation/app.js
+++ DOM_manipulation/app.js
@@ -1,15 +1,27 @@
 var button = document.getElementById('enter')
 var input = document.getElementById("user-input");
 var ul = document.querySelector('ul')
-var li = document.getElementsByClassName('item-todo');
+var span_items = document.querySelectorAll('span');
+var button_remove_items = document.querySelectorAll('.button-remove');
 
 function inputLength() {
     return input.value.length;
 }
 
 function createListElement() {
+    var button_remove = document.createElement('button');
+    button_remove.classList.add("button-remove");
+    button_remove.appendChild(document.createTextNode("Quitar"));
+    button_remove.addEventListener('click', removeListElement);
+
+    var span = document.createElement('span');
+    span.addEventListener('click', toggleDoneClassItem);
+    span.appendChild(document.createTextNode(input.value));
+    
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
+    li.appendChild(span);
+    li.appendChild(button_remove);
+    
     ul.appendChild(li);
     input.value = '';
 }
@@ -26,10 +38,24 @@ function addListAfterKeyPress(event){
     }
 }
 
+function toggleDoneClassItem(){
+    // console.log(this);
+    this.classList.toggle('done');
+}
+
+function removeListElement(){
+    //remove element
+    this.parentElement.remove();
+}
 
 /* events */
 button.addEventListener('click', addListAfterClick);
 input.addEventListener('keypress', addListAfterKeyPress);
-li.addEventListener('click', function(){
-    console.log('item clickeado');
-})
+// li.addEventListener('click', toggleDoneClassItem);
+span_items.forEach(element => {
+    element.addEventListener('click', toggleDoneClassItem);
+});
+//add event to all already created remove buttons
+button_remove_items.forEach(element => {
+    element.addEventListener('click', removeListElement);
+});
