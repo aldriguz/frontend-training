4c9f762f40265519731d2386988b220b123e57fc
Kevin Alama <alama1992@gmail.com>
2020-09-13 00:51:02 -0500

    advance arrays

--- js/exercise_array.js
+++ js/exercise_array.js
@@ -7,19 +7,19 @@ const array = [
       items: ["ball", "book", "pen"]
     },
     {
-      username: "becky",
+      username: "becky!",
       team: "blue",
       score: 10,
       items: ["tape", "backpack", "pen"]
     },
     {
-      username: "susy",
+      username: "susy?",
       team: "red",
       score: 55,
       items: ["ball", "eraser", "pen"]
     },
     {
-      username: "tyson",
+      username: "tyson!?",
       team: "green",
       score: 1,
       items: ["book", "pen"]
@@ -28,17 +28,40 @@ const array = [
   ];
   
   //Create an array using forEach that has all the usernames with a "!" to each of the usernames
+  const arrayExclamation = [];
+  const addExclamation = array.forEach((item) => {
+    arrayExclamation.push(`${item.username}!`);
+  });
+  console.log(arrayExclamation);
   
-  
+  const arrayExclamationFilter = [];
+  const filterExclamation = array.forEach((item) => {
+    if(item.username.includes("!")){
+        arrayExclamationFilter.push(item.username);
+    }    
+  });
+  console.log(arrayExclamationFilter);
+
   //Create an array using map that has all the usernames with a "? to each of the usernames
-  
+  const mapArrayQuestionMark = array.map(item => {
+      if(item.username.includes("?")){
+        return item;
+      }          
+  });
+  console.log(mapArrayQuestionMark);
   
   //Filter the array to only include users who are on team: red
-  
+  const filterArray = array.filter(item => item.team === "red");
+  console.log(filterArray);
   
   //Find out the total score of all users using reduce
-  
-  // (1), what is the value of i?
+  const reduceScore = array.reduce((totalScore, item) => {
+      return totalScore + item.score;
+  }, 0);
+  console.log("reduce score", reduceScore);
+
+
+  // (1), what is the value of i? index
   // (2), Make this map function pure:
   const arrayNum = [1, 2, 4, 5, 8, 9];
   const newArray = arrayNum.map((num, i) => {
@@ -47,5 +70,7 @@ const array = [
       return num * 2;
   })
   
+  const newArrayPure = arrayNum.map(num => num * 2);
+
   //BONUS: create a new list with all user information, but add "!" to the end of each items they own.
   
