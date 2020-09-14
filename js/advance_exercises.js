296864bc844382d94784b3ee895acc812efeef06
Kevin Alama <alama1992@gmail.com>
2020-09-14 00:48:22 -0500

    more fun from ES10

--- js/advance_exercises.js
+++ js/advance_exercises.js
@@ -37,6 +37,10 @@ compose(add1, add5)(10)
 // 2. determinism
 
 
+
+
+//-----------------------------------------------------
+
 // Solve the below problems:
 
 
@@ -65,7 +69,7 @@ powerOf100(2);
 //infinity
 
 
-
+//-----------------------------------------------------
 //----- ES8
 // Solve the below problems:
 
@@ -100,6 +104,8 @@ console.log(rabbit);
 // Read about what the second parameter does in padEnd and padStart
 turtle = turtle.trim().padEnd(9, '=');
 
+//solution: second parameter is the character that will be filled in
+
 
 // #3) Get the below object to go from:
 let obj = {
@@ -109,3 +115,55 @@ let obj = {
 }
 // to this:
 'my name is Rudolf the raindeer'
+
+var strFromObj = '';
+Object.entries(obj).map(value => {
+    strFromObj = strFromObj.concat(`${value[0]} ${value[1]} `);
+});
+
+console.log(strFromObj);
+
+
+//-----------------------------------------------------
+
+// Solve the below questions:
+
+// #1 Turn this array into a new array: [1,2,3,[4],[5]]. Bonus if you can do it on one line
+const array = [[1],[2],[3],[[[4]]],[[[5]]]]
+array.flat().flat();
+
+// #2 Turn this array into a new array: [ 'Hello young grasshopper!', 'you are', 'learning fast!' ]
+const greeting = [["Hello", "young", "grasshopper!"], ["you", "are"], ["learning", "fast!"]];
+greeting.flat();
+greeting.flatMap(item => item.join(' '));
+
+//#3 Turn the greeting array above into a string: 'Hello young grasshopper you are learning fast!'
+greeting.flatMap(item => item.join(' ')).join(' ');
+
+
+
+//#4 Turn the trapped 3 number into: [3]
+const trapped = [[[[[[[[[[[[[[[[[[[[[[[[[[3]]]]]]]]]]]]]]]]]]]]]]]]]];
+
+
+//#5 Clean up this email to have no whitespaces. Make the answer be in a single line (return a new string):
+const userEmail3 = '     cannotfillemailformcorrectly@gmail.com   '
+userEmail3.trimStart().trimEnd();
+
+
+//#6 Turn the below users (value is their ID number) into an array: [ [ 'user1', 18273 ], [ 'user2', 92833 ], [ 'user3', 90315 ] ]
+const users = { user1: 18273, user2: 92833, user3: 90315 }
+Object.entries(users);
+
+//#7 change the output array of the above to have the user's IDs multiplied by 2 -- Should output:[ [ 'user1', 36546 ], [ 'user2', 185666 ], [ 'user3', 180630 ] ]
+Object.entries(users).map(item => {
+    return [item[0], item[1] * 2];
+});
+
+
+//#8 change the output array of question #7 back into an object with all the users IDs updated to their new version. Should output: { user1: 36546, user2: 185666, user3: 180630 }
+const newUsers = Object.entries(users).map(item => {
+    return [item[0], item[1] * 2];
+});
+
+Object.fromEntries(newUsers);
--- js/new_enmascript.js
+++ js/new_enmascript.js
@@ -28,9 +28,12 @@ const fun = (a, b, c, d,) => {
 
 fun(1,2, 3, 4,)//stil valid
 
+
+/* 
 Object.values
 Object.entries
-Object.keys
+Object.keys 
+*/
 
 let obj = {
     username0: 'Santa',
@@ -38,3 +41,56 @@ let obj = {
     username2: 'Mr.Grinch'
 }
 
+
+Object.keys(obj).forEach((key, index) => {
+    console.log(key, obj[key]);
+});
+
+
+Object.values(obj).forEach(value => {
+    console.log(value);
+});
+
+
+Object.entries(obj).forEach(value => {
+    console.log(value);
+});
+
+
+Object.entries(obj).map(value => {
+    return value[1] + value[0].replace('username', '');
+});
+
+
+
+//ES10 (2019)
+//flat method that convert all to linear array
+const jurassicPark = [['🦖'],['🐤', '🐎'],['🦢', '🐄'],[[['🦖']]],[[['🦜']]]];
+jurassicPark.flat(50);
+
+const entrie = ['bob', 'sally',,,,,,'cindy'];
+entrie.flat(); //clean the empty ones
+
+const jurassicParkChaos = jurassicPark.flatMap(creature => creature + '🦖');
+jurassicParkChaos;
+
+//trimming 
+const userEmail = '              kalama@mymail.com';
+const userEmail2 = 'kalama@mymail.com              ';
+
+console.log(userEmail.trimStart());
+console.log(userEmail2.trimEnd())
+
+
+const userProfiles = [['commanderTom', 23], ['derekZlander', 24], ['aldriguz', 28]];
+Object.fromEntries(userProfiles);//convert to an object
+
+
+//try catch
+
+try {
+    bob + '!!'
+} catch (e) {
+    console.log('Error!!!');
+    console.log(e);
+}
