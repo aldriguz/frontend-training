69e6ebc84aec8e224649fc54f945382874ef5b8c
Kevin Alama <alama1992@gmail.com>
2020-09-13 23:42:13 -0500

    practicing ES8

--- js/advance_exercises.js
+++ js/advance_exercises.js
@@ -63,3 +63,49 @@ powerOf100(2);
 // #4) Useing your function from #3, put in the paramter 10000. What is the result?
 // Research for yourself why you get this result
 //infinity
+
+
+
+//----- ES8
+// Solve the below problems:
+
+// #1) Line up the Turtle and the Rabbit at the start line:
+const startLine = '     ||<- Start line';
+let turtle = '🐢';
+let rabbit = '🐇';
+
+// it should look like this:
+'     ||<- Start line'
+'       🐢'
+'       🐇'
+
+//solution
+var startLineLength = startLine.indexOf('-');
+turtle = turtle.padStart(startLineLength);
+rabbit = rabbit.padStart(startLineLength);
+
+
+// when you do:
+console.log(startLineLength);
+console.log(startLine);
+console.log(turtle);
+console.log(rabbit);
+
+
+
+
+
+
+// #2) What happens when you run turtle.trim().padEnd(9, '=') on the turtle variable
+// Read about what the second parameter does in padEnd and padStart
+turtle = turtle.trim().padEnd(9, '=');
+
+
+// #3) Get the below object to go from:
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'raindeer'
+}
+// to this:
+'my name is Rudolf the raindeer'
