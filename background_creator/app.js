a43570305197952041243da61b13235f6124cb08
Kevin Alama <alama1992@gmail.com>
2020-09-22 13:32:10 -0500

    using browserify

--- background_creator/app.js
+++ background_creator/app.js
@@ -1,3 +1,9 @@
+//import { without } from 'lodash';
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,3,8];
+console.log('answer', _.without(array, 3));
+
 var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
