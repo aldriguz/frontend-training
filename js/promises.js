9a4caeb27701d5d5194cccd98b48ededcea945e2
Kevin Alama <alama1992@gmail.com>
2020-10-17 11:15:31 -0500

    test of promises

--- js/promises.js
+++ js/promises.js
@@ -17,3 +17,16 @@ promise
         console.log(result2);
     })
     .catch(() => console.log('Error!!'));
+
+
+//this promise will return an object    
+let myPromiseObject = new Promise((resolve, reject) => {
+    setTimeout(function(){
+        resolve({success: true});
+    }, 200);
+});
+
+
+myPromiseObject.then((success) => {
+    console.log(success);
+});
