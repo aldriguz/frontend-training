19316d6fa696331341374d146e7df26eca9d2787
Kevin Alama <alama1992@gmail.com>
2021-01-01 14:41:01 -0500

    server.js return response in json format

--- node_http_server/server.js
+++ node_http_server/server.js
@@ -1,7 +1,27 @@
 const http = require('http');
 
-const server = http.createServer(() => {
-    console.log('listening!');
+const server = http.createServer((request, response) => {
+    //console.log('listening!');
+    //console.log('headers', request.headers);
+    //console.log('method', request.method);
+    //console.log('url', request.url);
+
+    /**
+     * html content type
+     */
+    //response.setHeader('content-Type', 'text/html');
+    //response.end('<h1>Helloooo</h1>');
+    
+    /**
+     * json content type
+     */
+    const user = {
+        name: 'John',
+        hobby: 'Stacking'
+    };
+    response.setHeader('content-Type', 'application/json');
+    response.end(JSON.stringify(user));
+
 });
 
 server.listen(3000)
