b1e7df5592c23b549bbd950cb034448b88ccc1ef
Kevin Alama <alama1992@gmail.com>
2021-01-17 16:49:00 -0500

    introduction to typescript

---
+++ typescript/script.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    return a + b;
+}
+var diff = function (a, b) {
+    return a - b;
+};
---
+++ typescript/script.ts
@@ -0,0 +1,7 @@
+function sum(a, b) {
+    return a + b;
+}
+
+const diff = (a, b) => {
+    return a - b;
+}
