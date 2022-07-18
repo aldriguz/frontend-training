5d509066c720238439c66defc86a3fa94b74eda8
Kevin Alama <alama1992@gmail.com>
2022-07-18 10:45:49 -0500

    components and arrays

--- starter-app/README.md
+++ starter-app/README.md
@@ -2,6 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.5.
 
+# Features
+- Angular components
+- CLI
+- ngFor
+- ngIf
+- 
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- starter-app/src/app/app.module.ts
+++ starter-app/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PopupComponent } from './popup/popup.component';
+import { PersonsComponent } from './persons/persons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PopupComponent
+    PopupComponent,
+    PersonsComponent
   ],
   imports: [
     BrowserModule,
